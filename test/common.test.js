31e8f99fdd0253bc2c125eace7d29943b5d02777
yejianxin <yejianxin@rightknights.com>
2021-03-08 22:19:48 +0800

    test: add some test unit

--- test/common.test.js
+++ test/common.test.js
@@ -1,6 +1,17 @@
-const { common } =  require('../lib/common')
+const { common } = require('../lib/common')
+const puppeteer = require('puppeteer')
 
-test('测试 flatten', () => {
-    expect(common.flatten([1,2,[3,4]])).toEqual([1,2,3,4])
-    expect(common.flatten([1,2,[3,4],[5,6,[7,8]]])).toEqual([1,2,3,4,5,6,7,8])
+test('测试数组扁平化函数：flatten', () => {
+    expect(common.flatten([1, 2, [3, 4]])).toEqual([1, 2, 3, 4])
+    expect(common.flatten([1, 2, [3, 4]])).toEqual([1, 2, 3, 4])
+    expect(common.flatten([1, 2, [3, 4], [5, 6, [7, 8]]])).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
+})
+
+test('baidu title is correct', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://baidu.com');
+    const title = await page.title();
+    expect(title).toBe('百度一下，你就知道');
+    await browser.close();
 })
